17c9455eb14b458f4ac010e3395d0f2818e50616
Ethan <ethan.credo@outlook.com>
2025-01-14 14:23:18 +0800

    create getComputerChoice function that returns computerChoice from Rock, Paper, Scissors

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
-function getComputerChoice() {
-    
+function getComputerChoice(computerChoice) {
+    computerChoice = Math.floor(Math.random() * 3);
+    if(computerChoice === 0) {
+        console.log("Rock");
+        return computerChoice;
+    } else if(computerChoice === 1) {
+        console.log("Paper");
+        return computerChoice;
+    } else if(computerChoice === 2) {
+        console.log("Scissors");
+        return computerChoice;
+    }
 }
+
+getComputerChoice();
