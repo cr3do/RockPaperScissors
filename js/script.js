fb82d6bc0729d9bd72be0e201da3c78a2acb096f
Ethan <ethan.credo@outlook.com>
2025-01-16 11:18:33 +0800

    add playRound function that compare player and computer choice

--- js/script.js
+++ js/script.js
@@ -1,15 +1,53 @@
-function getComputerChoice(computerChoice) {
-    computerChoice = Math.floor(Math.random() * 3);
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3);
     if(computerChoice === 0) {
         console.log("Rock");
-        return computerChoice;
+        return computerChoice = "Rock";
     } else if(computerChoice === 1) {
         console.log("Paper");
-        return computerChoice;
+        return computerChoice = "Paper";
     } else if(computerChoice === 2) {
         console.log("Scissors");
-        return computerChoice;
+        return computerChoice = "Scissors";
     }
 }
 
-getComputerChoice();
+// console.log(`Computer choice: ${getComputerChoice()}`);
+
+function getPlayerChoice() {
+    let playerChoice = parseInt(prompt("Input number to play: 0=rock, 1=paper, 2=scissors: "));
+    if(playerChoice === 0) {
+        console.log("Rock");
+        return playerChoice = "Rock";
+    } else if(playerChoice === 1) {
+        console.log("Paper");
+        return playerChoice = "Paper";
+    } else if(playerChoice === 2) {
+        console.log("Scissors");
+        return playerChoice = "Scissors";
+    } else {
+        console.log("Invalid choice.");
+        return playerChoice = "Invalid choice.";
+    }
+}
+
+// console.log(`Player choice: ${getPlayerChoice()}`);
+
+
+
+
+function playRound() {
+    let computerChoice = getComputerChoice();
+    let playerChoice = getPlayerChoice();
+
+    if(computerChoice == "Rock" && playerChoice == "Scissors" || computerChoice == "Paper" && playerChoice == "Rock" || computerChoice == "Scissors" && playerChoice == "Paper") {
+        console.log("Computer wins");
+    } 
+    else if(playerChoice == "Rock" && computerChoice == "Scissors" || playerChoice == "Paper" && computerChoice == "Rock" || playerChoice == "Scissors" && computerChoice == "Paper") {
+        console.log("Player wins");
+    } else {
+        console.log("Draw");
+    }
+}
+
+playRound();
