04fda49b0a677c1541538a6802ebf3677a20ac2e
Ethan <ethan.credo@outlook.com>
2025-02-06 15:26:11 +0800

    add results log in the ui using prepend

--- js/script.js
+++ js/script.js
@@ -23,6 +23,9 @@ btnRock.addEventListener("click", playGame);
 btnPaper.addEventListener("click", playGame);
 btnScissors.addEventListener("click", playGame);
 
+const resultList = document.getElementById("resultList");
+
+
 function playRound(playerChoice, computerChoice) {
 
     console.log(`Computer Choice: ${computerChoice}  |  Player Choice: ${playerChoice}`);
@@ -33,12 +36,27 @@ function playRound(playerChoice, computerChoice) {
     else if(computerChoice == "Rock" && playerChoice == "Scissors" || computerChoice == "Paper" && playerChoice == "Rock" || computerChoice == "Scissors" && playerChoice == "Paper") {
         console.log(`Computer wins! ${computerChoice} beats ${playerChoice}`);
         computerScore++;
+        
+        const resultMessage = document.createElement("li");
+        resultList.prepend(resultMessage);
+
+        resultMessage.textContent = `Computer wins! ${computerChoice} beats ${playerChoice}`;
     } 
     else if(playerChoice == "Rock" && computerChoice == "Scissors" || playerChoice == "Paper" && computerChoice == "Rock" || playerChoice == "Scissors" && computerChoice == "Paper") {
         console.log(`Player wins! ${playerChoice} beats ${computerChoice}`);
         playerScore++;
+
+        const resultMessage = document.createElement("li");
+        resultList.prepend(resultMessage);
+
+        resultMessage.textContent = `Player wins! ${playerChoice} beats ${computerChoice}`;
     } else {
         console.log("Draw");
+
+        const resultMessage = document.createElement("li");
+        resultList.prepend(resultMessage);
+
+        resultMessage.textContent = "Draw";
     }
 
     console.log(`Computer Score: ${computerScore}  |  Player Score: ${playerScore}`);
