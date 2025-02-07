662a87360bee5e8c8459dd5ec1692aca7f5b68d2
Ethan <ethan.credo@outlook.com>
2025-02-07 13:22:35 +0800

    add button images and display selection by image

--- js/script.js
+++ js/script.js
@@ -28,11 +28,37 @@ const computerScoreBoard = document.getElementById("computerScore");
 const computerSelectionBoard = document.getElementById("computerSelection");
 const playerScoreBoard = document.getElementById("playerScore");
 const playerSelectionBoard = document.getElementById("playerSelection");
+const imgComputerSelection = document.getElementById("imgComputerSelection");
+const imgPlayerSelection = document.getElementById("imgPlayerSelection");
 
 
 function playRound(playerChoice, computerChoice) {
 
     console.log(`Computer Choice: ${computerChoice}  |  Player Choice: ${playerChoice}`);
+
+    // display computer selection
+    if(computerChoice == "Rock") {
+        imgComputerSelection.src = "images/rocks.png";
+        computerSelectionBoard.appendChild(imgComputerSelection);
+    } else if(computerChoice == "Paper") {
+        imgComputerSelection.src = "images/paper.png";
+        computerSelectionBoard.appendChild(imgComputerSelection);
+    } else if(computerChoice == "Scissors") {
+        imgComputerSelection.src = "images/scissors.png";
+        computerSelectionBoard.appendChild(imgComputerSelection);
+    }
+
+    // display player selection
+    if(playerChoice == "Rock") {
+        imgPlayerSelection.src = "images/rocks.png";
+        playerSelectionBoard.appendChild(imgPlayerSelection);
+    } else if(playerChoice == "Paper") {
+        imgPlayerSelection.src = "images/paper.png";
+        playerSelectionBoard.appendChild(imgPlayerSelection);
+    } else if(playerChoice == "Scissors") {
+        imgPlayerSelection.src = "images/scissors.png";
+        playerSelectionBoard.appendChild(imgPlayerSelection);
+    }
 
     if(playerChoice == "Invalid") {
         console.log("Invalid");
@@ -45,11 +71,12 @@ function playRound(playerChoice, computerChoice) {
         computerScoreBoard.textContent = computerScore;
 
         // display computer and player selection
-        computerSelectionBoard.textContent = computerChoice;
-        playerSelectionBoard.textContent = playerChoice;
+        // computerSelectionBoard.textContent = computerChoice;
+        // playerSelectionBoard.textContent = playerChoice;
         
         // create new list tag then add result message in top/reverse order using prepend
         const resultMessage = document.createElement("li");
+        resultMessage.setAttribute("class", "red");
         resultList.prepend(resultMessage);
         resultMessage.textContent = `Computer wins! ${computerChoice} beats ${playerChoice}`;
     } 
@@ -61,19 +88,20 @@ function playRound(playerChoice, computerChoice) {
         playerScoreBoard.textContent = playerScore;
 
         // display computer and player selection
-        computerSelectionBoard.textContent = computerChoice;
-        playerSelectionBoard.textContent = playerChoice;
+        // computerSelectionBoard.textContent = computerChoice;
+        // playerSelectionBoard.textContent = playerChoice;
 
         // create new list tag then add result message in top/reverse order using prepend
         const resultMessage = document.createElement("li");
+        resultMessage.setAttribute("class", "green");
         resultList.prepend(resultMessage);
         resultMessage.textContent = `Player wins! ${playerChoice} beats ${computerChoice}`;
     } else {
         console.log("Draw");
 
         // display computer and player selection
-        computerSelectionBoard.textContent = computerChoice;
-        playerSelectionBoard.textContent = playerChoice;
+        // computerSelectionBoard.textContent = computerChoice;
+        // playerSelectionBoard.textContent = playerChoice;
 
         // create new list tag then add result message in top/reverse order using prepend
         const resultMessage = document.createElement("li");
